808ca28f290395f7fe96d39f946281086d0efdc2
Bas Machielsen <basmachielsen@live.nl>
2023-05-10 12:54:14 +0200

    Complete adding and interpolating wealth

--- vothbeh_test/code/7_wealth_calculation.R
+++ vothbeh_test/code/7_wealth_calculation.R
@@ -106,9 +106,84 @@ calculate_back_deflate_assets <- function(row){
 }
 
 # this works perfectly
-test <- data |> 
+data <- data |> 
   rowwise() |> 
   mutate(defw = list(calculate_back_deflate_assets(cur_data())))
 
 
-# now, implement with constant rebalancing
+# now, implement with yearly rebalancing
+calculate_back_deflate_assets_rebalance <- function(row){
+  # compute the amounts
+  re <- row$re
+  safe <- row$misc + row$cash + row$dugobo
+  dush <- row$dush
+  duprbo <- row$duprbo
+  fogobo <- row$fogobo
+  foprbo <- row$foprbo
+  fosh <- row$fosh
+  debt <- row$debt
+  # initialize total
+  ta <- re + safe + dush + duprbo + fogobo + foprbo + fosh
+  # initialize shares: 
+  # order: re, safe, dush, duprbo, fogobo, foprbo, fosh
+  shares <- c(re/ta, safe/ta, dush/ta, duprbo/ta, fogobo/ta, foprbo/ta, fosh/ta)
+  
+  vote_year <- row$date |> year()
+  death_year <- row$death_date |> year()
+  
+  for(i in death_year:vote_year){
+    back_d <- dutch |>
+      filter(year == i) |> 
+      summarize(safe = safe*exp(-safe_tr),
+                dush = dush*exp(-eq_tr),
+                duprbo = duprbo*exp(-ltrate/100),
+                re = re*exp(-housing_rent_rtn),
+                debt = debt*exp(-safe_tr)
+                )
+    
+    back_f <- foreign |> 
+      filter(year == i) |>
+      summarize(fogobo = fogobo*exp(-safe_tr),
+                foprbo = foprbo*exp(-ltrate/100),
+                fosh = fosh*exp(-eq_tr)
+      )
+    
+    # calculate total
+    total_back <- sum(back_d$safe, back_d$dush, back_d$duprbo, back_d$re, na.rm = T) + 
+    sum(back_f$fogobo, back_f$foprbo, back_f$fosh, na.rm = TRUE)
+    # rebalance
+    total <- total_back * shares
+    # write back to original variables
+    re <- total_back[1]
+    safe <- total_back[2]
+    dush <- total_back[3]
+    duprbo <- total_back[4]
+    fogobo <- total_back[5]
+    foprbo <- total_back[6]
+    fosh <- total_back[7]
+    
+    debt <- back_d$debt
+    
+  }
+  
+  # deflate
+  cpi <- deflator |> 
+    filter(year == vote_year) |>
+    select(cpi_1900) |>
+    pull()
+  
+  out <- (total_back - debt)/cpi
+  
+  return(out)
+  
+}
+
+data <- data |> 
+  rowwise() |> 
+  mutate(defw_rb = list(calculate_back_deflate_assets_rebalance(cur_data())))
+
+data <- data |> 
+  unnest(defw)
+
+# write to csv
+write_csv2(data, './vothbeh_test/data/voting/voth_wealth.csv')
