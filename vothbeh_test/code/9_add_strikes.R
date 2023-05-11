61c0ad89bf24aa1d22c2ab207e67a729eaa1d2a8
Bas Machielsen <basmachielsen@live.nl>
2023-05-11 12:36:28 +0200

    add final dataset with all variables

--- vothbeh_test/code/9_add_strikes.R
+++ vothbeh_test/code/9_add_strikes.R
@@ -1,6 +1,6 @@
 # add strikes
-library(tidyverse); library(Hmisc)
-
+library(tidyverse); library(Hmisc) #warning, this package overrides dplyr's summarize
+source('./vothbeh_test/code/x_helpers_step4.R')
 data <- read_csv2('./vothbeh_test/data/voting/vot_beh_wealth_instr.csv')
 # strikes url
 #download.file("https://datasets.iisg.amsterdam/api/access/datafile/9841",
@@ -23,6 +23,41 @@ strikes_db <- strikes |>
 #find an overview of number of strikes per year
 strikes_db <- strikes_db |> 
   group_by(Amsterdam.Code, JAAR) |> 
-  count()
+  count() |>
+  ungroup()
 
-# tomorrow: retrieve the municipality-mapping and incorporate the strikes in year-1
+# retrieve the municipality-mapping and incorporate the strikes in year-1
+## merge these to get the district -> gemeentenaam -> amco (hdng identifier) match
+dm <- read_csv('./vothbeh_test//data/district/district_municipality_time_table.csv')
+key <- read_delim('./vothbeh_test/data/district/key_elections_hdng_amco.csv')
+mapping <- dm |> left_join(key, by = c('gemeentenaam' = 'name_elections'))
+
+
+find_number_of_strikes <- function(row){
+  
+  year <- row$date |> year()
+  district <- row$district |> strip_roman()
+  
+  if(is.na(district)){
+    return(NA)
+  }
+  municipalities <- find_municipalities(district, year)
+  
+  strikes_db |> 
+    filter(is.element(Amsterdam.Code, municipalities),
+           between(JAAR, year-1, year)) |> 
+    dplyr::summarize(amount_of_strikes = sum(n)) |>
+    pull(amount_of_strikes)
+  
+}
+
+# write this variable
+data <- data |> 
+  rowwise() |> 
+  mutate(amount_of_strikes = list(find_number_of_strikes(cur_data())))
+
+data <- data |> 
+  unnest(amount_of_strikes)
+
+#write to csv
+write_csv2(data, "./vothbeh_test/data/analysis/dataset_final.csv")
